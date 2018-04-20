06c0faa22aa3b87a0df6229f567143fd5323dd20
Konstantin Pronin <troyandex@gmail.com>
2018-04-20 11:28:15 +0400

    pre final

--- test.php
+++ test.php
@@ -9,22 +9,10 @@ foreach ($file_list as $key => $file) {
     }
 }
 $questions = $test['questions']; // масив из вопросов с ответами выбраного (по гет) теста
-$result_true_array = array(); // обьявляем масив с верными ответами в исходном тесте
-
-echo "<pre>"; // смотрю что в массивах пост и верные
-    echo "Выбрал пользователь: <br>";
-    print_r($_POST);
-    echo "<br>Верные ответы: <br>";
-    $correct_arr[0] = "test";
-    print_r($correct_arr);
-echo "</pre>";
-
-
 ?>
 
 <!DOCTYPE html>
 <html lang="ru">
-
 <head>
     <title>2.2 «Обработка форм» - Тест </title>
     <meta charset="UTF-8">
@@ -36,50 +24,81 @@ echo "</pre>";
         .answer:hover {text-decoration: underline;}
     </style>
 </head>
-
 <body>
-<div>
-    <div class="container">
-        <h2>Меню:</h2>
-        <ul>
-            <li><a href="admin.php">Форма загрузки тестов</a></li>
-            <li><a href="list.php">Список тестов</a></li>
-        </ul>
+<div class="container">
+    <h2>Меню:</h2>
+    <ul>
+        <li><a href="admin.php">Форма загрузки тестов</a></li>
+        <li><a href="list.php">Список тестов</a></li>
+    </ul>
 
-        <h2>Тест: <?=$test['title']?></h2>
-        <form method="post">
-            <fieldset>
-                <?php
+    <h2>Тест: <?=$test['title']?></h2>
+    <form method="post">
+        <fieldset>
+            <?php
+//            echo "<br><pre>";
+//            print_r($_POST);
+//            echo "<br></pre>";
 
-                foreach ($questions as $key1 => $number) :  // для каждого вопроса по порядку
-                    $question = $number['question'];// массив с вопросами
-                    $answers = $number['answers']; // массив с ответами
+            // кол-во ответов:
+            $post_true = 0; // верно пользователь
+            $post_false = 0; // ошибочно пользователь
+            $result_true = 0; // должно быть верных
 
-                    echo "<h4>Вопрос: $question</h4>"; // выводим вопрос
-                    foreach ($answers as $key2 => $option) : // выводим каждый ответ и собираем правильные
-
-                        if ($option['result'] ===  true) {
-                            $index = "$key1-$key2";
-                            global $correct_arr;
-                            $correct_arr[$index] = $option['answer'] ; // если верен - добавляем к массиву
-                        }
-                        ?>
-                        <label class="answer">
-                            <!-- $key1 - вопрос, $key2 - ответ, -->
-                            <input type="checkbox" name="<?php echo $key1 . "-" . $key2;?>" value="<?=$option['answer'];?>">
-                            <?=$option['answer'];?>
-                        </label>
-
-                    <?php
-                    endforeach; // заканчиваем цикл с выводом всех и подсчетом верных ответов
-                endforeach;
+            foreach ($questions as $key1 => $number) : // для каждого вопроса по порядку
+                $question = $number['question']; // массив с вопросами
+                $answers[] = $number['answers']; // массив с ответами
                 ?>
-                <br>
-                <input class="button" type="submit" value="Отправить">
-            </fieldset>
-        </form>
+                <h4>Вопрос: <?=$question;?></h4>
+                <?php
+                foreach ($answers[$key1] as $key2 => $item) :
+                    if ($item['result'] === true) {
+                        $result_true++; // плюсуем количество верных ответов
+                    };
 
-    </div>
+                    if (count($_POST) > 0) { // если есть пост - проверяем
+                        $answers_key = "$key1-$key2"; // ключ текущего ответа для сверки
+                        if (isset($_POST[$answers_key])) { // если есть пост с таким же как вопрос индексом
+                            if ($item['result'] === true) { // и он верный
+                                $post_true++; // плюсуем верно
+                            } else {
+                                $post_false++; // иначе плюсуем ошибку
+                            }
+                        }
+                    };
+                    ?>
+                    <label class="answer">
+                        <input type="checkbox" name="<?php echo $key1."-".$key2;?>" value="<?php echo $key1."-".$key2;?>">
+                        <?=$item['answer'];?>
+                    </label>
+                <?php
+                endforeach; // конец обработки вопроса
+                ?>
+
+            <?php
+            endforeach;
+
+            // Сравниваем и выводим результат
+//            echo "<br>";
+//            echo "<br> post_true = " . $post_true . "<br>";
+//            echo "<br> post_false =" . $post_false . "<br>";
+//            echo "<br> result_true = " . $result_true . "<br>";
+
+            // если есть ответы - выводим результат
+            if (count($_POST) > 0) {
+                if ($post_true === $result_true && $post_false === 0) {
+                    echo '<h4>Результат: Правильно!</h4>';
+                }elseif ($post_true > 0 && $post_false > 0) {
+                    echo '<h4>Результат: Почти угадали (попробуйте еще)</h4>';
+                }else{
+                    echo '<h4>Результат: Совсем не то</h4>';
+                }
+            }
+            ?>
+        </fieldset>
+        <input class="button" type="submit" value="Отправить">
+    </form>
+
 </div>
 </body>
 </html>
